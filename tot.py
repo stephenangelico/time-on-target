73015b599772f5f0bb9733aa9a516a82c2ae6d3c
Stephen Angelico <raaff35lightning@gmail.com>
2024-12-21 11:10:25 +1100

    tot: Re-enable backlight as soft PWM

--- tot.py
+++ tot.py
@@ -14,7 +14,7 @@ import time
 
 # Init LCD
 lcd_rs = digitalio.DigitalInOut(board.D2)
-lcd_rw = 3
+# LCD RW (pin 5) is pulled to ground as we never need to read from the display
 lcd_en = digitalio.DigitalInOut(board.D4)
 lcd_d4 = digitalio.DigitalInOut(board.D27)
 lcd_d5 = digitalio.DigitalInOut(board.D22)
@@ -22,16 +22,21 @@ lcd_d6 = digitalio.DigitalInOut(board.D23)
 lcd_d7 = digitalio.DigitalInOut(board.D24)
 lcd_columns = 20
 lcd_rows = 4
+lcd_backlight = 3
+pwm_freq = 50
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(lcd_rw, GPIO.OUT, initial=GPIO.LOW)
-#lcd_backlight = digitalio.DigitalInOut(board.D21) # Works but doesn't supply enough power
+GPIO.setup(lcd_backlight, GPIO.OUT)
+global pwm
+pwm = GPIO.PWM(lcd_backlight, pwm_freq)
+pwm.start(0)
 lcd = character_lcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows)
-#lcd.backlight = True
 lcd.blink = False
 
 lcd.clear()
 lcd.message = "Hello world!"
+pwm.ChangeDutyCycle(50)
 time.sleep(30)
 lcd.clear()
+pwm.ChangeDutyCycle(0)
 GPIO.cleanup()
