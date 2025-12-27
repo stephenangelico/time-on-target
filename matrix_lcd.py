359c298152ad3f73f56c85a03c57707f072cd240
Stephen Angelico <raaff35lightning@gmail.com>
2025-12-27 13:23:55 +1100

    matrix_lcd: Make some helper functions and flesh out the init process

--- matrix_lcd.py
+++ matrix_lcd.py
@@ -1,26 +1,102 @@
 # Matrix LCD test/library
+import time
+from enum import IntEnum
 import RPi.GPIO as GPIO
 
 # Pin GPIO numbers
-PIN_RS = 2 # Register Select or Data/Instruction
-PIN_RW = 3 # Read/Write
-PIN_EN = 4 # Enable (pulsed only)
-PIN_DB0 = 25 # Data bit 0
-PIN_DB1 = 8  # Data bit 1
-PIN_DB2 = 7  # Data bit 2
-PIN_DB3 = 1  # Data bit 3
-PIN_DB4 = 0  # Data bit 4
-PIN_DB5 = 5  # Data bit 5
-PIN_DB6 = 6  # Data bit 6
-PIN_DB7 = 13 # Data bit 7
-PIN_CS1 = 14 # Column Select 1 (columns 1-64)
-PIN_CS2 = 15 # Column Select 2 (columns 65-128)
-PIN_RST = 18 # Reset
+class Pin(IntEnum):
+	RS = 2 # Register Select or Data/Instruction
+	RW = 3 # Read/Write
+	EN = 4 # Enable (pulsed only)
+	DB0 = 25 # Data bit 0
+	DB1 = 8  # Data bit 1
+	DB2 = 7  # Data bit 2
+	DB3 = 1  # Data bit 3
+	DB4 = 0  # Data bit 4
+	DB5 = 5  # Data bit 5
+	DB6 = 6  # Data bit 6
+	DB7 = 13 # Data bit 7
+	CS1 = 14 # Column Select 1 (columns 1-64)
+	CS2 = 15 # Column Select 2 (columns 65-128)
+	RST = 18 # Reset
+
+data_pins = (Pin.DB7, Pin.DB6, Pin.DB5, Pin.DB4, Pin.DB3, Pin.DB2, Pin.DB1, Pin.DB0)
+
+def pulse_enable():
+	GPIO.output(Pin.EN, 1)
+	time.sleep(0.000001)
+	GPIO.output(Pin.EN, 0)
+
+def set_rw_read():
+	GPIO.output(Pin.RW, 1)
+	for pin in data_pins:
+		GPIO.setup(pin, GPIO.IN)
+
+def set_rw_write():
+	GPIO.output(Pin.RW, 0)
+	for pin in data_pins:
+		GPIO.setup(pin, GPIO.OUT)
+	# TODO: Merge these functions
+
+def set_cs(chip):
+	GPIO.output(Pin.CS1, chip == 1 or chip == 3)
+	GPIO.output(Pin.CS2, chip == 2 or chip == 3)
+	
+def status_read():
+	GPIO.output(Pin.RS, 0)
+	set_rw_read()
+	# Check each segment controller one at a time
+	set_cs(1)
+	pulse_enable()
+	busy1 = GPIO.input(Pin.DB7)
+	display1 = GPIO.input(Pin.DB5)
+	resetting1 = GPIO.input(Pin.DB4)
+	set_cs(2)
+	pulse_enable()
+	busy2 = GPIO.input(Pin.DB7)
+	display2 = GPIO.input(Pin.DB5)
+	resetting2 = GPIO.input(Pin.DB4)
+	set_rw_write()
+	if busy1 or busy2: busy = 1; else busy = 0
+	if display1 or display2: display = 1; else display = 0
+	# display will return 0 if on, therefore if either display is off, it will be 1
+	# This will signal that one or both displays need to be turned on
+	if resetting1 or resetting2: resetting = 1; else resetting = 0
+	return busy, display, resetting
+
+def set_data_bits(databyte):
+	for pin, state in zip(data_pins, "%08b" % databyte):
+		GPIO.output(pin, state == "1")
+		# Byte must be given in binary. Bits are stringified and zipped with data pins
+		# For any pin, if state is the string "0", comparing against the string "1"
+		# will be false and thus set the pin state to low.
 
 def init():
-	GPIO.setmode(BCM)
-	GPIO.setwarnings(False)
-	# TODO: Figure out the init process
-	# Do we need to first pulse the reset line? What (if anything) can we read?
 	# Refer to https://github.com/crystalfontz/Neotec-NT7108/blob/main/NT7108/NT7108.ino
 	# (Arduino code) for examples - written in C++ so needs to be converted
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setwarnings(False)
+	for pin in Pin:
+		GPIO.setup(pin, GPIO.OUT, initial=GPIO.LOW)
+	GPIO.output(Pin.RST, 1)
+	while "still booting":
+		ready_state = status_read()
+		if not ready_state[2]:
+			break
+		else:
+			time.sleep(0.0001) # 100usec, well over the 60usec max busy time
+	GPIO.output(Pin.RS, 0)
+	set_cs(3)
+	set_rw_write()
+	set_data_bits(0b00111111) # Display on
+	pulse_enable()
+	# TODO: Check busy?
+	set_data_bits(0b01000000) # Y addr 0
+	pulse_enable()
+	# 
+	set_data_bits(0b10111000) # X addr (page) 0
+	pulse_enable()
+	# 
+	set_data_bits(0b11000000) # Z addr (display start line)
+	pulse_enable()
+	# 
