5429669c7d508c1295706d3572fdc11b063e7f13
Stephen Angelico <raaff35llightning@gmail.com>
2026-03-28 11:06:09 +1100

    font_small: Make / wider to prevent untidy aliasing

--- font_small.py
+++ font_small.py
@@ -23,6 +23,15 @@ FONT = {
 		" XX ",
 		" XX ",
 	],
+	"/": [
+		"      X",
+		"     X ",
+		"    X  ",
+		"   X   ",
+		"  X    ",
+		" X     ",
+		"X      ",
+	],
 }
 
 def _make_font(template):
@@ -48,11 +57,11 @@ X X X   X     X    XXX  X  X  XXXX  XXXX     X   XXX   XXXX
 XX  X   X    X        X XXXXX     X X   X    X  X   X     X
 X   X   X   X     X   X    X  X   X X   X   X   X   X     X
  XXX   XXX  XXXXX  XXX     X   XXX   XXX    X    XXX      X""")
-_make_font("""()/!?
-   X   X       X  XX    XXX 
-  X     X     X  XXXX  X   X
- X       X    X  XXXX     X  
- X       X   X    XX     X  
- X       X  X            X  
-  X     X   X     XX        
-   X   X   X      XX     X  """)
+_make_font("""()!?
+   X   X     XX    XXX 
+  X     X   XXXX  X   X
+ X       X  XXXX     X  
+ X       X   XX     X  
+ X       X          X  
+  X     X    XX        
+   X   X     XX     X  """)
