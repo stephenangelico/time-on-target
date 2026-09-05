a12cf738b70492988b7269f5df5ec56bcd92abc1
Stephen Angelico <raaff35lightning@gmail.com>
2026-09-05 11:30:33 +1000

    tot: Use only one time.strftime call

--- tot.py
+++ tot.py
@@ -229,8 +229,9 @@ def clock_ticker():
 		first_row = font_big.ASCENDER + font_big.BASE - 1 # Zero-base addressing
 		second_row = first_row + font_small.ADVANCEMENT
 		third_row = first_row + font_small.ADVANCEMENT * 2
-		matrix_lcd.draw_text(0, first_row, time.strftime("%H:%M"), font=font_big)
-		matrix_lcd.draw_text(90, first_row, time.strftime("%S"), font=font_large)
+		hrmin, sec = time.strftime("%H:%M %S").split()
+		matrix_lcd.draw_text(0, first_row, hrmin, font=font_big)
+		matrix_lcd.draw_text(90, first_row, sec, font=font_large)
 		matrix_lcd.draw_text(0, second_row, line1)
 		matrix_lcd.draw_text(0, third_row, line2)
 		if anim_chevron_time:
