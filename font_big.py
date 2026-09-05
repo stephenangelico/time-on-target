b6ffbff19fcdae6de0291f85554695151063932b
Stephen Angelico <raaff35lightning@gmail.com>
2026-09-05 10:21:42 +1000

    font_big: Reduce size of font to 3x font_small

--- font_big.py
+++ font_big.py
@@ -1,44 +1,37 @@
 # Broken out into its own file since these will be fairly large
 
-ASCENDER = 12
-BASE = 16
+ASCENDER = 9
+BASE = 12
 DESCENDER = 0
 LEADING = 0 # Note: This is not how far it leads (pron leed) but how much lead is used (pron ledd).
 LINEHEIGHT = ASCENDER + BASE + DESCENDER
 ADVANCEMENT = LINEHEIGHT + LEADING
-LETTERSPACING = 4 # Pixels between characters horizontally
+LETTERSPACING = 3 # Pixels between characters horizontally
 
 FONT = {
-	" ": [" " * 24] * 28, # Critical - without this for unknown characters, everything will bomb
+	" ": [" " * 18] * 21, # Critical - without this for unknown characters, everything will bomb
 	":": [ # TODO
-		"                ",
-		"                ",
-		"                ",
-		"                ",
-		"                ",
-		"                ",
-		"    XXXXXXXX    ",
-		"    XXXXXXXX    ",
-		"    XXXXXXXX    ",
-		"    XXXXXXXX    ",
-		"    XXXXXXXX    ",
-		"    XXXXXXXX    ",
-		"                ",
-		"                ",
-		"                ",
-		"                ",
-		"    XXXXXXXX    ",
-		"    XXXXXXXX    ",
-		"    XXXXXXXX    ",
-		"    XXXXXXXX    ",
-		"    XXXXXXXX    ",
-		"    XXXXXXXX    ",
-		"                ",
-		"                ",
-		"                ",
-		"                ",
-		"                ",
-		"                ",
+		"        ",
+		"        ",
+		"        ",
+		"        ",
+		"  XXXX  ",
+		"  XXXX  ",
+		"  XXXX  ",
+		"  XXXX  ",
+		"  XXXX  ",
+		"        ",
+		"        ",
+		"        ",
+		"  XXXX  ",
+		"  XXXX  ",
+		"  XXXX  ",
+		"  XXXX  ",
+		"  XXXX  ",
+		"        ",
+		"        ",
+		"        ",
+		"        ",
 	],
 }
 
@@ -49,31 +42,24 @@ def _make_font(n, template):
 # NOTE: Keep the digits all at max width, even if they don't all use it. This
 # keeps the display stable during ticking.
 _make_font(24, """0123456789
-    XXXXXXXXXXXX                XXXX                XXXXXXXXXXXX            XXXXXXXXXXXX                    XXXX        XXXXXXXXXXXXXXXXXXXX        XXXXXXXXXXXX        XXXXXXXXXXXXXXXXXXXX        XXXXXXXXXXXX            XXXXXXXXXXXX        
-    XXXXXXXXXXXX                XXXX                XXXXXXXXXXXX            XXXXXXXXXXXX                    XXXX        XXXXXXXXXXXXXXXXXXXX        XXXXXXXXXXXX        XXXXXXXXXXXXXXXXXXXX        XXXXXXXXXXXX            XXXXXXXXXXXX        
-    XXXXXXXXXXXX                XXXX                XXXXXXXXXXXX            XXXXXXXXXXXX                    XXXX        XXXXXXXXXXXXXXXXXXXX        XXXXXXXXXXXX        XXXXXXXXXXXXXXXXXXXX        XXXXXXXXXXXX            XXXXXXXXXXXX        
-    XXXXXXXXXXXX                XXXX                XXXXXXXXXXXX            XXXXXXXXXXXX                    XXXX        XXXXXXXXXXXXXXXXXXXX        XXXXXXXXXXXX        XXXXXXXXXXXXXXXXXXXX        XXXXXXXXXXXX            XXXXXXXXXXXX        
-XXXX            XXXX        XXXXXXXX            XXXX            XXXX    XXXX            XXXX            XXXXXXXX        XXXX                    XXXX            XXXX                    XXXX    XXXX            XXXX    XXXX            XXXX    
-XXXX            XXXX        XXXXXXXX            XXXX            XXXX    XXXX            XXXX            XXXXXXXX        XXXX                    XXXX            XXXX                    XXXX    XXXX            XXXX    XXXX            XXXX    
-XXXX            XXXX        XXXXXXXX            XXXX            XXXX    XXXX            XXXX            XXXXXXXX        XXXX                    XXXX            XXXX                    XXXX    XXXX            XXXX    XXXX            XXXX    
-XXXX            XXXX        XXXXXXXX            XXXX            XXXX    XXXX            XXXX            XXXXXXXX        XXXX                    XXXX            XXXX                    XXXX    XXXX            XXXX    XXXX            XXXX    
-XXXX        XXXXXXXX            XXXX                        XXXX                        XXXX        XXXX    XXXX        XXXX                    XXXX                                    XXXX    XXXX            XXXX    XXXX            XXXX    
-XXXX        XXXXXXXX            XXXX                        XXXX                        XXXX        XXXX    XXXX        XXXX                    XXXX                                    XXXX    XXXX            XXXX    XXXX            XXXX    
-XXXX        XXXXXXXX            XXXX                        XXXX                        XXXX        XXXX    XXXX        XXXX                    XXXX                                    XXXX    XXXX            XXXX    XXXX            XXXX    
-XXXX        XXXXXXXX            XXXX                        XXXX                        XXXX        XXXX    XXXX        XXXX                    XXXX                                    XXXX    XXXX            XXXX    XXXX            XXXX    
-XXXX    XXXX    XXXX            XXXX                    XXXX                XXXXXXXXXXXX        XXXX        XXXX        XXXXXXXXXXXXXXXX        XXXXXXXXXXXXXXXX                    XXXX            XXXXXXXXXXXX            XXXXXXXXXXXXXXXX    
-XXXX    XXXX    XXXX            XXXX                    XXXX                XXXXXXXXXXXX        XXXX        XXXX        XXXXXXXXXXXXXXXX        XXXXXXXXXXXXXXXX                    XXXX            XXXXXXXXXXXX            XXXXXXXXXXXXXXXX    
-XXXX    XXXX    XXXX            XXXX                    XXXX                XXXXXXXXXXXX        XXXX        XXXX        XXXXXXXXXXXXXXXX        XXXXXXXXXXXXXXXX                    XXXX            XXXXXXXXXXXX            XXXXXXXXXXXXXXXX    
-XXXX    XXXX    XXXX            XXXX                    XXXX                XXXXXXXXXXXX        XXXX        XXXX        XXXXXXXXXXXXXXXX        XXXXXXXXXXXXXXXX                    XXXX            XXXXXXXXXXXX            XXXXXXXXXXXXXXXX    
-XXXXXXXX        XXXX            XXXX                XXXX                                XXXX    XXXXXXXXXXXXXXXXXXXX                    XXXX    XXXX            XXXX                XXXX        XXXX            XXXX                    XXXX    
-XXXXXXXX        XXXX            XXXX                XXXX                                XXXX    XXXXXXXXXXXXXXXXXXXX                    XXXX    XXXX            XXXX                XXXX        XXXX            XXXX                    XXXX    
-XXXXXXXX        XXXX            XXXX                XXXX                                XXXX    XXXXXXXXXXXXXXXXXXXX                    XXXX    XXXX            XXXX                XXXX        XXXX            XXXX                    XXXX    
-XXXXXXXX        XXXX            XXXX                XXXX                                XXXX    XXXXXXXXXXXXXXXXXXXX                    XXXX    XXXX            XXXX                XXXX        XXXX            XXXX                    XXXX    
-XXXX            XXXX            XXXX            XXXX                    XXXX            XXXX                XXXX        XXXX            XXXX    XXXX            XXXX            XXXX            XXXX            XXXX                    XXXX    
-XXXX            XXXX            XXXX            XXXX                    XXXX            XXXX                XXXX        XXXX            XXXX    XXXX            XXXX            XXXX            XXXX            XXXX                    XXXX    
-XXXX            XXXX            XXXX            XXXX                    XXXX            XXXX                XXXX        XXXX            XXXX    XXXX            XXXX            XXXX            XXXX            XXXX                    XXXX    
-XXXX            XXXX            XXXX            XXXX                    XXXX            XXXX                XXXX        XXXX            XXXX    XXXX            XXXX            XXXX            XXXX            XXXX                    XXXX    
-    XXXXXXXXXXXX            XXXXXXXXXXXX        XXXXXXXXXXXXXXXXXXXX        XXXXXXXXXXXX                    XXXX            XXXXXXXXXXXX            XXXXXXXXXXXX                XXXX                XXXXXXXXXXXX                        XXXX    
-    XXXXXXXXXXXX            XXXXXXXXXXXX        XXXXXXXXXXXXXXXXXXXX        XXXXXXXXXXXX                    XXXX            XXXXXXXXXXXX            XXXXXXXXXXXX                XXXX                XXXXXXXXXXXX                        XXXX    
-    XXXXXXXXXXXX            XXXXXXXXXXXX        XXXXXXXXXXXXXXXXXXXX        XXXXXXXXXXXX                    XXXX            XXXXXXXXXXXX            XXXXXXXXXXXX                XXXX                XXXXXXXXXXXX                        XXXX    
-    XXXXXXXXXXXX            XXXXXXXXXXXX        XXXXXXXXXXXXXXXXXXXX        XXXXXXXXXXXX                    XXXX            XXXXXXXXXXXX            XXXXXXXXXXXX                XXXX                XXXXXXXXXXXX                        XXXX    """)
+   XXXXXXXXX            XXX            XXXXXXXXX         XXXXXXXXX               XXX      XXXXXXXXXXXXXXX      XXXXXXXXX      XXXXXXXXXXXXXXX      XXXXXXXXX         XXXXXXXXX      
+   XXXXXXXXX            XXX            XXXXXXXXX         XXXXXXXXX               XXX      XXXXXXXXXXXXXXX      XXXXXXXXX      XXXXXXXXXXXXXXX      XXXXXXXXX         XXXXXXXXX      
+   XXXXXXXXX            XXX            XXXXXXXXX         XXXXXXXXX               XXX      XXXXXXXXXXXXXXX      XXXXXXXXX      XXXXXXXXXXXXXXX      XXXXXXXXX         XXXXXXXXX      
+XXX         XXX      XXXXXX         XXX         XXX   XXX         XXX         XXXXXX      XXX               XXX         XXX               XXX   XXX         XXX   XXX         XXX   
+XXX         XXX      XXXXXX         XXX         XXX   XXX         XXX         XXXXXX      XXX               XXX         XXX               XXX   XXX         XXX   XXX         XXX   
+XXX         XXX      XXXXXX         XXX         XXX   XXX         XXX         XXXXXX      XXX               XXX         XXX               XXX   XXX         XXX   XXX         XXX   
+XXX      XXXXXX         XXX                  XXX                  XXX      XXX   XXX      XXX               XXX                           XXX   XXX         XXX   XXX         XXX   
+XXX      XXXXXX         XXX                  XXX                  XXX      XXX   XXX      XXX               XXX                           XXX   XXX         XXX   XXX         XXX   
+XXX      XXXXXX         XXX                  XXX                  XXX      XXX   XXX      XXX               XXX                           XXX   XXX         XXX   XXX         XXX   
+XXX   XXX   XXX         XXX               XXX            XXXXXXXXX      XXX      XXX      XXXXXXXXXXXX      XXXXXXXXXXXX               XXX         XXXXXXXXX         XXXXXXXXXXXX   
+XXX   XXX   XXX         XXX               XXX            XXXXXXXXX      XXX      XXX      XXXXXXXXXXXX      XXXXXXXXXXXX               XXX         XXXXXXXXX         XXXXXXXXXXXX   
+XXX   XXX   XXX         XXX               XXX            XXXXXXXXX      XXX      XXX      XXXXXXXXXXXX      XXXXXXXXXXXX               XXX         XXXXXXXXX         XXXXXXXXXXXX   
+XXXXXX      XXX         XXX            XXX                        XXX   XXXXXXXXXXXXXXX               XXX   XXX         XXX            XXX      XXX         XXX               XXX   
+XXXXXX      XXX         XXX            XXX                        XXX   XXXXXXXXXXXXXXX               XXX   XXX         XXX            XXX      XXX         XXX               XXX   
+XXXXXX      XXX         XXX            XXX                        XXX   XXXXXXXXXXXXXXX               XXX   XXX         XXX            XXX      XXX         XXX               XXX   
+XXX         XXX         XXX         XXX               XXX         XXX            XXX      XXX         XXX   XXX         XXX         XXX         XXX         XXX               XXX   
+XXX         XXX         XXX         XXX               XXX         XXX            XXX      XXX         XXX   XXX         XXX         XXX         XXX         XXX               XXX   
+XXX         XXX         XXX         XXX               XXX         XXX            XXX      XXX         XXX   XXX         XXX         XXX         XXX         XXX               XXX   
+   XXXXXXXXX         XXXXXXXXX      XXXXXXXXXXXXXXX      XXXXXXXXX               XXX         XXXXXXXXX         XXXXXXXXX            XXX            XXXXXXXXX                  XXX   
+   XXXXXXXXX         XXXXXXXXX      XXXXXXXXXXXXXXX      XXXXXXXXX               XXX         XXXXXXXXX         XXXXXXXXX            XXX            XXXXXXXXX                  XXX   
+   XXXXXXXXX         XXXXXXXXX      XXXXXXXXXXXXXXX      XXXXXXXXX               XXX         XXXXXXXXX         XXXXXXXXX            XXX            XXXXXXXXX                  XXX   """)
