d828ce9312e6777101e68b5fef0d8658afa14729
Stephen Angelico <raaff35llightning@gmail.com>
2026-09-25 12:27:44 +1000

    pwm_test: Fix range and add cleanup

--- pwm_test.py
+++ pwm_test.py
@@ -4,17 +4,27 @@ import RPi.GPIO as GPIO
 
 PIN_PWM = 22 # Physical pin 15
 FREQ = 50
+pwm = None
 
 def test_backlight():
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setwarnings(False)
 	GPIO.setup(PIN_PWM, GPIO.OUT)
+	global pwm
 	pwm = GPIO.PWM(PIN_PWM, FREQ)
 	pwm.start(0)
-	for n in range(100, step=10):
+	for n in range(0, 101, 10):
 		print("Brightness level:", n)
 		pwm.ChangeDutyCycle(n)
 		time.sleep(1)
 
+def cleanup():
+	global pwm
+	pwm.ChangeDutyCycle(0)
+	pwm.stop()
+	pwm = None # Must be *before* cleanup due to bug in lgpio
+	GPIO.cleanup()
+
 if __name__ == "__main__":
 	test_backlight()
+	cleanup()
